3a26d97cdfff4955b0020738c123869af5a8cbf6
sproshchaev <sproshchaev@gmail.com>
2024-07-11 14:45:03 +0500

    feat: main

--- 03-concurrency/src/main/java/ru/otus/Main.java
+++ 03-concurrency/src/main/java/ru/otus/Main.java
@@ -1,9 +1,40 @@
 package ru.otus;
 
 public class Main {
-
     public static void main(String[] args) {
-
+        Main main1 = new Main();
+        Main main2 = new Main();
+        new Thread(() -> main1.method1()).start();
+        new Thread(() -> main1.method1()).start();
     }
-
+    // Синхронизированный метод 1
+    public synchronized void method1() {
+        System.out.println(Thread.currentThread().getName() + " method1() started");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getName() + " method1() ended");
+    }
+    // Синхронизированный метод 2
+    public synchronized void method2() {
+        System.out.println(Thread.currentThread().getName() + " method2() started");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getName() + " method2() ended");
+    }
+    // Этот метод без синхронизации
+    public void method3() {
+        System.out.println(Thread.currentThread().getName() + " method3() started");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getName() + " method3() ended");
+    }
 }
