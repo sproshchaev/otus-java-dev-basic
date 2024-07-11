4440b46d4fa3cee246a404cb7af372c3a7f2d60b
sproshchaev <sproshchaev@gmail.com>
2024-07-11 15:36:04 +0500

    feat: add method()

--- 03-concurrency/src/main/java/ru/otus/example/two/Main.java
+++ 03-concurrency/src/main/java/ru/otus/example/two/Main.java
@@ -5,8 +5,36 @@ package ru.otus.example.two;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    private final Object monitor = new Object();
 
+    public static void main(String[] args) {
+        Main main1 = new Main();
+        new Thread(() -> main1.method()).start();
+        new Thread(() -> main1.method()).start();
     }
+
+    public void method() {
+        try {
+            // 1) Эту часть потоки выполняют параллельно
+            System.out.println(Thread.currentThread().getName() + " Not syn: Start");
+            for (int i = 0; i < 3; i++) {
+                System.out.println(".");
+                Thread.sleep(300);
+            }
+            System.out.println(Thread.currentThread().getName() + " Not syn: End");
+            // 2) Здесь начинается критическая секция. Потоки выполняют последовательно
+            synchronized (monitor) {
+                System.out.println(Thread.currentThread().getName() + " Syn: Start");
+                for (int i = 0; i < 3; i++) {
+                    System.out.println(".");
+                    Thread.sleep(300);
+                }
+                System.out.println(Thread.currentThread().getName() + " Syn: End");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
