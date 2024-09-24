1d07d021f0dd7138c737796f38b26486abd999cd
sproshchaev <sproshchaev@gmail.com>
2024-09-24 15:11:09 +0500

    upd

--- 04-tests-logs/src/main/java/com/prosoft/Main.java
+++ 04-tests-logs/src/main/java/com/prosoft/Main.java
@@ -2,6 +2,12 @@ package com.prosoft;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        System.out.println("1 + 1 = " + simpleCalculator.add(1, 1));
+        System.out.println("1 - 1 = " + simpleCalculator.subtract(1, 1));
+        System.out.println("1 * 1 = " + simpleCalculator.multiply(1, 1));
+        System.out.println("1 / 1 = " + simpleCalculator.divide(1, 1));
+
     }
 }
