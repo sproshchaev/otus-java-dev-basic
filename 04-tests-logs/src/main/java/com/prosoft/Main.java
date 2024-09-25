fcf97a737cce0c1a72f96a6277fb3190e82a3ac3
sproshchaev <sproshchaev@gmail.com>
2024-09-25 17:16:48 +0500

    upd

--- 04-tests-logs/src/main/java/com/prosoft/Main.java
+++ 04-tests-logs/src/main/java/com/prosoft/Main.java
@@ -4,10 +4,33 @@ public class Main {
     public static void main(String[] args) {
 
         SimpleCalculator simpleCalculator = new SimpleCalculator();
-        System.out.println("1 + 1 = " + simpleCalculator.add(1, 1));
-        System.out.println("1 - 1 = " + simpleCalculator.subtract(1, 1));
-        System.out.println("1 * 1 = " + simpleCalculator.multiply(1, 1));
-        System.out.println("1 / 1 = " + simpleCalculator.divide(1, 1));
 
+        // Тест для метода add
+        if (simpleCalculator.add(1, 1) == 2) {
+            System.out.println("Test add(1, 1) passed.");
+        } else {
+            System.out.println("Test add(1, 1) failed.");
+        }
+
+        // Тест для метода subtract
+        if (simpleCalculator.subtract(1, 1) == 0) {
+            System.out.println("Test subtract(1, 1) passed.");
+        } else {
+            System.out.println("Test subtract(1, 1) failed.");
+        }
+
+        // Тест для метода multiply
+        if (simpleCalculator.multiply(1, 1) == 1) {
+            System.out.println("Test multiply(1, 1) passed.");
+        } else {
+            System.out.println("Test multiply(1, 1) failed.");
+        }
+
+        // Тест для метода divide
+        if (simpleCalculator.divide(1, 1) == 1) {
+            System.out.println("Test divide(1, 1) passed.");
+        } else {
+            System.out.println("Test divide(1, 1) failed.");
+        }
     }
 }
