e5d5f5f05a4faccd7a2f7179c26466d48222779a
sproshchaev <sproshchaev@gmail.com>
2024-11-25 08:42:45 +0500

    upd

---
+++ 00-maven-live/src/test/java/com/prosoft/SimpleCalculatorTest.java
@@ -0,0 +1,11 @@
+package com.prosoft;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SimpleCalculatorTest {
+
+    @Test
+    void add() {
+    }
+}
