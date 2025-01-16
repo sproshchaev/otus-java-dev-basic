f78c0b16190f6a9018e1c0758857dc83d0820e23
sproshchaev <sproshchaev@gmail.com>
2025-01-16 21:22:30 +0500

    start

--- 02-java-io/src/main/java/ru/otus/Main.java
+++ 02-java-io/src/main/java/ru/otus/Main.java
@@ -1,8 +1,13 @@
 package ru.otus;
 
+/**
+ * Занятие «Jаva IO. Часть 2 \\ ДЗ» 16-01-2025
+ */
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Hello, Java!");
 
     }
 
