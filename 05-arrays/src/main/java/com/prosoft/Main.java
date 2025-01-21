e20938a3ba2a08665021623f5e866a4d19316478
sproshchaev <sproshchaev@gmail.com>
2025-01-21 21:22:23 +0500

    start

---
+++ 05-arrays/src/main/java/com/prosoft/Main.java
@@ -0,0 +1,10 @@
+package com.prosoft;
+
+/**
+ * Java-Basic «Основы языка Java. Часть 2 \\ ДЗ» 21-01-2025
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
