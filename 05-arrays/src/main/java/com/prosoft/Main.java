b372867ae712550497232a06db6c0c8da5a1a2e3
sproshchaev <sproshchaev@gmail.com>
2025-01-22 11:42:09 +0500

    refactor

--- 05-arrays/src/main/java/com/prosoft/Main.java
+++ 05-arrays/src/main/java/com/prosoft/Main.java
@@ -9,92 +9,127 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Перебор с фиксированным количеством итераций
-        //for (int i = 0; i < 5; i++) {
-        //    System.out.println("i=" + i);
-        //}
+        System.out.println("\n#1 Перебор с фиксированным количеством итераций:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i=" + i);
+        }
 
-        // Перебор чётных чисел
-//        for (int i = 0; i <= 10; i += 2) {
-//            System.out.println("i=" + i);
-//        }
+        System.out.println("\n#2 Перебор чётных чисел:");
+        for (int i = 0; i <= 10; i += 2) {
+            System.out.println("i=" + i);
+        }
 
-        // Перебор в обратном порядке
-//        for (int i = 100; i >= 0; i -= 10) {
-//            System.out.println("i=" + i);
-//        }
+        System.out.println("\n#3 Перебор в обратном порядке:");
+        for (int i = 100; i >= 0; i -= 10) {
+            System.out.println("i=" + i);
+        }
 
-        // long
-        // float
-//        for (float j = 0.1f; j < 5.0f; j = j + 0.2f) {
-//            System.out.println("j=" + j);
-//        }
+        System.out.println("\n#4 Цикл for с переменной типа long:");
+        for (long j = 1L; j < 10L; j++) {
+            System.out.println("j=" + j);
+        }
 
-        // Бесконечный цикл
+        System.out.println("\n#5 Цикл for с переменной типа float:");
+        for (float k = 0.1f; k < 5.0f; k = k + 0.2f) {
+            System.out.println("k=" + k);
+        }
+
+        System.out.println("\n#6 Цикл for с переменной типа char:");
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            System.out.println("Current letter: " + letter);
+        }
+
+        System.out.println("\n#7 Бесконечный цикл:");
 //       for (;;) {
 //           System.out.println("Hello World!");
 //       }
 
+        System.out.println("\n#8 Операции над элементами массива:");
         int[] arr1 = {1, 2, 3}; // new int [3];
         int[] arr2 = {4, 5, 6};
-        // System.out.println("arr1[0] + arr2[0]=" + (arr1[0] + arr2[0]));
-        // doSomethingWithArray(arr1);
-        // doSomethingWithArray(new int[]{10, 20, 30});
-        // doSomethingWithArrayVararg(2, 3, 4, 5, 6, 1);
-        // doSomethingWithArrayVararg( 2, arr1);
-        // compareArrays();
-        // printArrays();
-        // incrementEvenPositionElements();
-        // zerifyElements();
-        // hardCase1();
-        // countElements();
-        // fillArrayHalfByHalf();
-        // sumArray();
-        // fillArray();
+        System.out.println("arr1[0] + arr2[0]=" + (arr1[0] + arr2[0]));
 
-        // Пример передачи параметра в метод по значению (примитивы)
+        System.out.println("\n#10 Длина и получение значений элементов массива:");
+        doSomethingWithArray(arr1);
+
+        System.out.println("\n#11 Длина и получение значений элементов массива:");
+        doSomethingWithArray(new int[]{10, 20, 30});
+
+        System.out.println("\n#12 Использование переменного числа аргументов (varargs):");
+        doSomethingWithArrayVararg(2, 3, 4, 5, 6, 1);
+
+        System.out.println("\n#13 Использование переменного числа аргументов (varargs):");
+        doSomethingWithArrayVararg( 2, arr1);
+
+        System.out.println("\n#14 Сравнение элементов двух массивов:");
+        compareArrays();
+
+        System.out.println("\n#15 Вывод элементов массива в консоль:");
+        printArrays();
+
+        System.out.println("\n#16 Увеличить элементы четных индексов на 1:");
+        incrementEvenPositionElements();
+
+        System.out.println("\n#17 Обнуление элементов массива по значению:");
+        zerifyElements();
+
+        System.out.println("\n#18 Определить количество ячеек, в которых следующим элементом идет число на 1 больше:");
+        hardCase1();
+
+        System.out.println("\n#19 Количество четных и положительных чисел в массиве:");
+        countElements();
+
+        System.out.println("\n#20 Заполнение половины массива (левая и правая):");
+        fillArrayHalfByHalf();
+
+        System.out.println("\n#21 Сумма элементов массива:");
+        sumArray();
+
+        System.out.println("\n#22 Заполнение массива определенным значением:");
+        fillArray();
+
+
+        System.out.println("\n#23 Пример передачи параметра в метод по значению (примитивы):");
         int i = 1;
         System.out.println("i before getByValue(i)=" + i); // 1
-        getByValue(i); // значение аргумента увеличиваем на i = i + 1 внутри метода
+        getByValue(i); // значение аргумента увеличиваем на i = i + 1 внутри метода getByValue()
         System.out.println("i after getByValue(i)=" + i); // 1 - значение не изменилось, так как для примитивов передается копия значения
 
-        // Пример передачи параметра в метод по ссылке (объекты)
+
+        System.out.println("\n#24 Пример передачи параметра в метод по ссылке (объекты):");
         System.out.println("arr1 before getByRef(arr1)=" + Arrays.toString(arr1)); // [1, 2, 3]
-        getByRef(arr1); // обнуляем все значения элементов в массиве
+        getByRef(arr1); // обнуляем все значения элементов в массиве в методе getByRef()
         System.out.println("arr1 after getByRef(arr1)=" + Arrays.toString(arr1)); // [0, 0, 0] - значение изменилось, так как для объектов передается ссылка на объект
 
     }
 
     public static void doSomethingWithArray(int[] arr) {
-        System.out.println("Длина массива =" + arr.length);
-        System.out.println("Первый элемент=" + arr[0]); // Первый элемент
-        System.out.println("Последний элемент=" + arr[arr.length - 1]);
+        System.out.println("Длина массива = " + arr.length);
+        System.out.println("Первый элемент = " + arr[0]);
+        System.out.println("Последний элемент = " + arr[arr.length - 1]);
     }
 
     public static void doSomethingWithArrayVararg(int x, int... arr) {
         System.out.println("Первый аргумент =" + x);
-        System.out.println("Длина массива =" + arr.length);
-        System.out.println("Первый элемент=" + arr[0]); // Первый элемент
-        System.out.println("Последний элемент=" + arr[arr.length - 1]);
+        System.out.println("Длина массива = " + arr.length);
+        System.out.println("Первый элемент = " + arr[0]);
+        System.out.println("Последний элемент = " + arr[arr.length - 1]);
     }
 
-    // Сверка двух массивов
     public static void compareArrays() {
         int[] arr = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4};
         int[] arr2 = {1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4};
         int counter = 0;
         for (int i = 0; i < Integer.min(arr.length, arr2.length); i++) {
-            //System.out.println("i=" + i);
             if (arr[i] == arr2[i]) {
                 counter++;
             }
         }
-        System.out.println("arr=" + Arrays.toString(arr)); // через Arrays.toString()
-        System.out.println("arr2=" + Arrays.toString(arr2)); // через Arrays.toString()
-        System.out.println("counter=" + counter);
+        System.out.println("arr = " + Arrays.toString(arr));
+        System.out.println("arr2 = " + Arrays.toString(arr2));
+        System.out.println("Число совпадений = " + counter);
     }
 
-    // Как вывести в консоль массивы
     public static void printArrays() {
         int[] arr = {1, 2, 4, 4, 4, 5, 6, 2, 3, 5, 6, 8};
         // Вывод через цикл
@@ -102,11 +137,10 @@ public class Main {
             System.out.print(arr[i] + ", ");
         }
         System.out.println("");
-        // через Arrays.toString()
+        // Вывод через Arrays.toString()
         System.out.println("arr=" + Arrays.toString(arr));
     }
 
-    // Как увеличить элементы на четных позициях на 1
     public static void incrementEvenPositionElements() {
         int[] arr = {1, 2, 3, 4, 5};
         for (int i = 0; i < arr.length; i++) {
@@ -118,10 +152,9 @@ public class Main {
         System.out.println("arr=" + Arrays.toString(arr));
     }
 
-    // Как занулить определенные элементы массива
     private static void zerifyElements() {
         int[] arr = {1, 2, 3, 4, 4, 5, 5};
-        // Зануление по значению 4 и 5
+        // Обнуление элементов по значению 4 и 5
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 4 || arr[i] == 5) {
                 arr[i] = 0;
@@ -143,7 +176,7 @@ public class Main {
         System.out.println("counter=" + counter);
     }
 
-    // Количество четных чисел и положительных
+    // Количество четных и положительных чисел в массиве
     private static void countElements() {
         int[] arr = {1, -1, 1, 1, -1, 1, 1, 2, 2, 2, 2, 2};
         int evenNumbersCount = 0;
