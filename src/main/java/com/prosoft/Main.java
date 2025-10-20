9e86c2857348a1271793421a95df32a62570ff24
sproshchaev <sproshchaev@gmail.com>
2025-10-20 19:27:07 +0500

    add varargs examples

--- src/main/java/com/prosoft/Main.java
+++ src/main/java/com/prosoft/Main.java
@@ -6,7 +6,27 @@ package com.prosoft;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // Поиск максимального значения в массиве
+
+        int[] temperature = {-5, 3, 12, -2};
+
+        // инициализация итогового значения первым элементом
+        int maxTemp = temperature[0];
+
+        for (int i =0; i < temperature.length; i++) {
+
+            // проверяю не больше ли текущий элемент?
+            if (temperature[i] > maxTemp) {
+                maxTemp = temperature[i];
+            } // if
+
+        } // for
+
+        System.out.println("Максимальная температура = " + maxTemp);
+
+
     }
+
 
 }
